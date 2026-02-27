84ba2749a0a393767b09e5e17e4d6a168ab5510c
Antonio Vitor Araujo <webaholicson@gmail.com>
2026-02-26 20:03:46 -0600

    Initial project structure

---
+++ src/test/java/com/superstar_rewards/app/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superstar_rewards.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
